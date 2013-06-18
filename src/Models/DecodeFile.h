90f0dc37e7e71c739289c6e278e6daa0b20d02f1
Rahul Shardha <rahul.shardha@gmail.com>
2013-06-18 15:28:53 -0400

    Function Declarations

---
+++ src/Models/DecodeFile.h
@@ -0,0 +1,14 @@
+/*
+ * DecodeFile.h
+ *
+ *  Created on: 2013-06-18
+ *      Author: Rahul
+ */
+
+#ifndef DECODEFILE_H_
+#define DECODEFILE_H_
+
+
+
+
+#endif /* DECODEFILE_H_ */
--- src/Models/Encrypt.h
+++ src/Models/Encrypt.h
@@ -7,7 +7,25 @@
 
 #ifndef ENCRYPT_H_
 #define ENCRYPT_H_
+#include <vector>
+#include <string>
 
+using namespace std;
+
+class encrypt{
+public:
+	encrypt();
+	encrypt(string bits);
+	encrypt(string bits, vector<string> decodeFile);
+
+	bool readFile();
+	bool writeFile();
+	bool generateDecodeFile();
+
+private:
+	string mDataBits;
+	vector<string> mDecodeFile;
+};
 
 
 
